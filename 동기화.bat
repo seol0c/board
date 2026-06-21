3ab37ce34d16bfce024b0b9d288f1d9d0dc55e39
seol0c <youngchanseol@gmail.com>
2026-06-21 15:21:30 +0900

    fix: ASCII messages in sync bat (Korean was garbled in cmd)

--- 동기화.bat
+++ 동기화.bat
@@ -1,17 +1,16 @@
 @echo off
-chcp 65001 >nul
 cd /d "%~dp0"
 echo.
-echo === board 게시판 동기화 ===
+echo ===== board sync =====
 echo.
-echo [1/2] 깃허브 변경 내려받기...
+echo [1/2] pull from github ...
 git pull
 echo.
-echo [2/2] 내 변경 올리기...
+echo [2/2] push my changes ...
 git add -A
 git commit -m "update %date% %time%"
 git push
 echo.
-echo 완료! 1~2분 뒤 https://seol0c.github.io/board/ 에 반영됩니다.
+echo Done. Open https://seol0c.github.io/board/ after 1-2 min.
 echo.
 pause
